6db77e8aefbabcac145667138415e1f449bcb36a
filossoof <filossoof@yandex.ru>
2025-01-12 19:35:59 +0500

    Update module_1_6.py

--- module_1_6.py
+++ module_1_6.py
@@ -9,8 +9,8 @@ print(my_dict)
 
 my_set = {24, 9, 1, 9, 2, 7, 2, 7, 27, 7, 9, 8, 28, 3, 'умер'}
 print(my_set)
-print(my_set.add(25))
-print(my_set.add('неизвестно'))
-print(my_set.add('возможно'))
-print(my_set.discard(28))
+my_set.add(25)
+my_set.add('неизвестно')
+my_set.add('возможно')
+my_set.discard(28)
 print(my_set)
